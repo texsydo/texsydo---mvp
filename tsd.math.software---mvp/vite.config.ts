f5be40498b4fbc25cf2eef58fa476923f9221d62
Tobias Briones <tbriones@mathswe.com>
2024-09-24 22:26:51 -0600

    Update tsconfig files form template

--- tsd.math.software---mvp/vite.config.ts
+++ tsd.math.software---mvp/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     resolve: {
         alias: {
             "@": resolve(__dirname, "src"),
+            "@app": resolve(__dirname, "src"),
         },
     },
 });
