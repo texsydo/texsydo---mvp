ce18906aab25d8a079315fc8122157ff1a010173
Tobias Briones <tbriones@mathswe.com>
2024-09-24 23:46:05 -0600

    Update vitest.config.ts form template

--- tsd.math.software---mvp/vitest.config.ts
+++ tsd.math.software---mvp/vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vitest/config";
+import viteConfig from "./vite.config.ts";
 
 export default defineConfig({
+    ...viteConfig,
     test: {
         globals: true,
         environment: "jsdom",
