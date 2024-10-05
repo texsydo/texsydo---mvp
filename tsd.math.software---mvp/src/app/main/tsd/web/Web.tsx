33c23cc5c0f64fee8980efd3e1effaa62bc84189
Tobias Briones <tbriones@mathswe.com>
2024-10-04 19:28:26 -0600

    Update section web with base Texsydo Web features

--- tsd.math.software---mvp/src/app/main/tsd/web/Web.tsx
+++ tsd.math.software---mvp/src/app/main/tsd/web/Web.tsx
@@ -4,23 +4,58 @@
 
 import "./Web.css";
 import webIcon from "@app/assets/texsydo-web.svg";
-import { Heading } from "@app/main/Heading.tsx";
+import { Heading, SubSubHeading } from "@app/main/Heading.tsx";
 import TerminalOutput
     from "@app/main/tsd/components/TerminalOutput/TerminalOutput.tsx";
 import { faTerminal } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ReactNode } from "react";
+import { MathJax } from "better-react-mathjax";
+import { Fragment, ReactNode } from "react";
 import { SubHeading } from "../../Heading.tsx";
 
 type InlineCodeProps = {
     children: ReactNode;
+    backgroundColor?: string;
 }
 
-function InlineCode({ children }: InlineCodeProps) {
+function InlineCode({ children, backgroundColor }: InlineCodeProps) {
     return <>
-        <code className="code language-plaintext highlighter-rouge">
+        <code
+            className="code language-plaintext highlighter-rouge"
+            style={ { backgroundColor } }
+        >
             { children }
         </code>
+    </>;
+}
+
+type InlineCodeMapProps = {
+    maps: Record<string, string>
+}
+
+function InlineCodeMap({ maps }: InlineCodeMapProps) {
+    const entries = Object.entries(maps);
+
+    const newItem = ([ from, to ]: string[], index: number) => (
+        <Fragment key={ index }>
+            <div className="grid-item">
+                <InlineCode>{ from }</InlineCode>
+            </div>
+
+            <div className="grid-item">
+                <MathJax>{ "`\\to`" }</MathJax>
+            </div>
+
+            <div className="grid-item">
+                <InlineCode backgroundColor="#BBDEFB">{ to }</InlineCode>
+            </div>
+        </Fragment>
+    );
+
+    return <>
+        <div className="pb-2 word-map-container">
+            { entries.map(newItem) }
+        </div>
     </>;
 }
 
@@ -94,12 +129,6 @@ function Web() {
                     <p>Mathematical Text to Web Article</p>
 
                     <p>
-                        Texsydo Web models mathematical articles as source code
-                        of a domain-specific system, which transforms into web
-                        articles adhering to MathSwe standards.
-                    </p>
-
-                    <p>
                         Web articles are modern, portable, easy to write, and
                         deploy with minimal overhead or setup.
                     </p>
@@ -108,23 +137,41 @@ function Web() {
 
                     <p>Texsydo Web Operations</p>
 
+                    <p>
+                        Operations provide commands to create, build, and deploy
+                        mathematical articles in your domain-specific system.
+                    </p>
+
+                    <p>
+                        ⚠️ Texsydo Web is a challenging project, so its current
+                        MVP versions do not guarantee API stability for now.
+                    </p>
+
+                    <SubSubHeading id="new-article" title="New Article" />
+
+                    <p>Creating Domain Aware Articles</p>
+
                     <Command
                         command="tsd-web create { entry-id } { class_1, class_2, ..., class_n }"
                         caption="New Article"
                     />
 
-                    <p>
-                        For example:
-                        <div>
-                            Entry ID
-                            = <InlineCode>automating-the-platform-operations-and-beyond-2023-08-31</InlineCode>
-                        </div>
-                        <div>
-                            Classes
-                            = <InlineCode>mathswe, eng, automation, platform,
-                                          ops</InlineCode>
-                        </div>
-                    </p>
+                    <div className="text-center mb-4">
+                        <strong>Create Article Example</strong>
+
+                        <ul className="text-start">
+                            <li>
+                                Entry ID
+                                is <InlineCode>automating-the-platform-operations-and-beyond-2023-08-31</InlineCode>.
+                            </li>
+                            <li>
+                                Classes are
+                                <InlineCode>
+                                    mathswe, eng, automation, platform, ops
+                                </InlineCode>.
+                            </li>
+                        </ul>
+                    </div>
 
                     <TerminalOutput
                         outputs={ [
@@ -149,6 +196,198 @@ function Web() {
                         |                       └── index.md
                         ` }
                     </SnippetBlock>
+
+                    <SubSubHeading
+                        id="listing-articles"
+                        title="Listing Articles"
+                    />
+
+                    <p>Repository Articles</p>
+
+                    <Command
+                        command="tsd-web entries"
+                        caption="Entires Command"
+                    />
+
+                    <SubSubHeading
+                        id="building-an-article"
+                        title="Building an Article"
+                    />
+
+                    <p>Markdown to Static Site</p>
+
+                    <Command
+                        command="tsd-web build { target } (jekyll)"
+                        caption="Build Command"
+                    />
+
+                    <p>
+                        ⚠️ Texsydo Web MVP will drop support for Jekyll later
+                        when it reaches a more mature stage.
+                    </p>
+
+                    <div className="text-center mb-4">
+                        <strong>Common Build Usage</strong>
+
+                        <ul className="text-start">
+                            <li>
+                                Target is the current
+                                branch <InlineCode>.</InlineCode>.
+                            </li>
+
+                            <li>
+                                The <InlineCode>jekyll</InlineCode> flag builds
+                                intermediate code to a static site.
+                            </li>
+
+                            <li>
+                                Run <InlineCode>tsd-web build .
+                                                jekyll</InlineCode>.
+                            </li>
+                        </ul>
+                    </div>
+
+                    <SubSubHeading
+                        id="serving-an-article"
+                        title="Serving an Article"
+                    />
+
+                    <p>Development Server</p>
+
+                    <Command
+                        command="tsd-web serve"
+                        caption="Serve Command"
+                    />
+
+                    <SubSubHeading
+                        id="deploying-an-article"
+                        title="Deploying an Article"
+                    />
+
+                    <p>GitHub Pages Deployment</p>
+
+                    <Command
+                        command="tsd-web deploy { entry-id }"
+                        caption="Deploy Command"
+                    />
+
+                    <TerminalOutput
+                        outputs={ [
+                            { icon: "gear", value: "Deploying" },
+                            "Checkout to branch main",
+                            "Update branch main",
+                            "",
+                            { icon: "gear", value: "Building" },
+                            "Build article automating-the-platform-operations-and-beyond-2023-08-31",
+                            "Checkout to branch gh-pages",
+                            "Update branch gh-pages",
+                            "Commiting build of automating-the-platform-operations-and-beyond-2023-08-31",
+                            "Add files to Git",
+                            "Commit files to Git",
+                            "",
+                            { icon: "gear", value: "Publishing" },
+                            "Push branch gh-pages to origin",
+                            "Checkout to branch main",
+                            "Deploy automating-the-platform-operations-and-beyond-2023-08-31",
+                        ] }
+                    />
+
+                    <div className="text-center">
+                        <p><strong>Production Article</strong></p>
+
+                        <a
+                            rel="noreferrer"
+                            target="_blank"
+                            href="https://blog.mathsoftware.engineer/automating-the-platform-operations-and-beyond-2023-08-31"
+                        >
+                            Automating the Platform Operations and Beyond
+                            (2023/08/31)
+                        </a>
+                    </div>
+
+                    <p>
+                        It deploys the given article from the main branch to the
+                        gh-pages one.
+                    </p>
+
+                    <SubSubHeading
+                        id="domain-dictionary"
+                        title="Domain Dictionary"
+                    />
+
+                    <p>Smart Text Transformations</p>
+
+                    <p>
+                        Declarative user dictionaries tell Texsydo the
+                        transformations from the low-level form of a word used
+                        as a syntax to the high-level word to render.
+                    </p>
+
+                    <p>
+                        Dictionaries keep the system domain-specific and,
+                        therefore, well-informed and efficient by removing
+                        redundant work prone to errors and writer burnout.
+                    </p>
+
+                    <SnippetBlock caption="Custom Dictionary Example">
+                        { `
+                        Dictionary(
+                        |   uppercase = setOf("bsd", "pr"),
+                        |   composed = mapOf(
+                        |       "mathswe" to listOf("math", "swe"),
+                        |       "github"  to listOf("git", "hub"),
+                        |   ),
+                        |   acronym = mapOf(
+                        |       "swam" to "Special Software and Models",
+                        |       "ddo"  to "Data-Driven Organizations",
+                        |   ),
+                        |   custom = mapOf(
+                        |       "intellij-idea" to "IntelliJ IDEA",
+                        |       "leds"          to "LEDs",
+                        |   )
+                        )
+                        ` }
+                    </SnippetBlock>
+
+                    <p><strong>Map of Words</strong></p>
+
+                    <ul className="flex-list">
+                        <li>
+                            <InlineCodeMap
+                                maps={ {
+                                    bsd: "BSD",
+                                    pr: "PR",
+                                } }
+                            />
+                        </li>
+
+                        <li>
+                            <InlineCodeMap
+                                maps={ {
+                                    mathswe: "MathSwe",
+                                    github: "GitHub",
+                                } }
+                            />
+                        </li>
+
+                        <li>
+                            <InlineCodeMap
+                                maps={ {
+                                    swam: "Special Software and Models",
+                                    ddo: "Data-Driven Organizations",
+                                } }
+                            />
+                        </li>
+
+                        <li>
+                            <InlineCodeMap
+                                maps={ {
+                                    "intellij-idea": "IntelliJ IDEA",
+                                    leds: "LEDs",
+                                } }
+                            />
+                        </li>
+                    </ul>
                 </div>
             </div>
         </section>
