01166b59b4d77d6a78724b444b6606a0703864ba
Tobias Briones <tbriones@mathswe.com>
2024-10-12 01:05:31 -0600

    Wrap presentation and images in section Fx
    It uses the screen width more efficiently.

--- tsd.math.software---mvp/src/app/main/tsd/fx/Fx.tsx
+++ tsd.math.software---mvp/src/app/main/tsd/fx/Fx.tsx
@@ -27,35 +27,40 @@ function Fx() {
     return <>
         <section className="bg-strip fx">
             <div className="bg">
-                <div className="wrap">
-                    <Heading
-                        id="fx"
-                        title="FX"
-                        icon={ { src: fxIcon, name: "FX" } }
-                    ></Heading>
+                <div className="wrap wrap-full-width">
+                    <div className="wrap-width">
+                        <Heading
+                            id="fx"
+                            title="FX"
+                            icon={ { src: fxIcon, name: "FX" } }
+                        ></Heading>
 
-                    <p>Text Effects</p>
+                        <p>Text Effects</p>
 
-                    <p>
-                        Texsydo renders text as mathematical art in effectful
-                        domains of article graphics.
-                    </p>
+                        <p>
+                            Texsydo renders text as mathematical art in
+                            effectful
+                            domains of article graphics.
+                        </p>
 
-                    <SubHeading
-                        id={ "development-article-cover" }
-                        title={ "Development Article Cover" }
-                    />
+                        <SubHeading
+                            id={ "development-article-cover" }
+                            title={ "Development Article Cover" }
+                        />
 
-                    <p>Continuous Development with Documentation Assistance</p>
+                        <p>Continuous Development with Documentation
+                           Assistance</p>
 
-                    <p>
-                        Texsydo FX currently supports generating cover images
-                        for blogs that update on Pull Requests (PRs) and project
-                        releases in MathSwe.
-                    </p>
+                        <p>
+                            Texsydo FX currently supports generating cover
+                            images
+                            for blogs that update on Pull Requests (PRs) and
+                            project
+                            releases in MathSwe.
+                        </p>
 
-                    <SnippetBlock caption="Generating a PR Cover Image">
-                        { `
+                        <SnippetBlock caption="Generating a PR Cover Image">
+                            { `
                         tsd-fx pr-cover preview \\
                         --profile-photo = profile.jpeg \\
                         --heading       = texsydo/@mvp \\
@@ -66,94 +71,116 @@ function Fx() {
                         --bg-color      = white \\
                         --bg            = msw-fractal.png
                         ` }
-                    </SnippetBlock>
+                        </SnippetBlock>
 
-                    <p>
-                        The above command will show a preview window with the
-                        rendered image.
-                    </p>
+                        <p>
+                            The above command will show a preview window with
+                            the
+                            rendered image.
+                        </p>
 
-                    <p>
-                        FX will save the image by removing
-                        the <InlineCode>preview</InlineCode> unnamed parameter
-                        and specifying
-                        an <InlineCode>output</InlineCode> argument with the
-                        destination path.
-                    </p>
+                        <p>
+                            FX will save the image by removing
+                            the <InlineCode>preview</InlineCode> unnamed
+                            parameter
+                            and specifying
+                            an <InlineCode>output</InlineCode> argument with the
+                            destination path.
+                        </p>
+                    </div>
 
-                    <Presentation
-                        id="texsydo-fx-_-pr-cover"
-                        title="PR Cover"
-                        sequence={ [
-                            {
-                                src: prCoverProfilePhoto,
-                                title: "Profile Photo",
-                            },
-                            {
-                                src: prCoverHeading,
-                                title: "Heading",
-                            },
-                            {
-                                src: prCoverSubheading,
-                                title: "Subheading",
-                            },
-                            {
-                                src: prCoverAbstract,
-                                title: "Abstract",
-                            },
-                            {
-                                src: prCoverDetails,
-                                title: "Details",
-                            },
-                            {
-                                src: prCoverSubdomain,
-                                title: "Subdomain",
-                            },
-                        ] }
-                    />
-
-                    <figure className={ "mx-lg-5 my-4" }>
-                        <img
-                            src={ prCoverWithCustomColor }
-                            alt="PR Cover with Custom Color"
+                    <div className="wrap-graphics">
+                        <Presentation
+                            id="texsydo-fx-_-pr-cover"
+                            title="PR Cover"
+                            sequence={ [
+                                {
+                                    src: prCoverProfilePhoto,
+                                    title: "Profile Photo",
+                                },
+                                {
+                                    src: prCoverHeading,
+                                    title: "Heading",
+                                },
+                                {
+                                    src: prCoverSubheading,
+                                    title: "Subheading",
+                                },
+                                {
+                                    src: prCoverAbstract,
+                                    title: "Abstract",
+                                },
+                                {
+                                    src: prCoverDetails,
+                                    title: "Details",
+                                },
+                                {
+                                    src: prCoverSubdomain,
+                                    title: "Subdomain",
+                                },
+                            ] }
                         />
-                        <figcaption>PR Cover with Custom Color</figcaption>
-                    </figure>
 
-                    <figure className={ "mx-lg-5 my-4" }>
-                        <img
-                            src={ prCoverInMathSweOps }
-                            alt="PR Cover in MathSwe Ops"
-                        />
-                        <figcaption>PR Cover in MathSwe Ops</figcaption>
-                    </figure>
+                        <div className="images">
+                            <div className="row">
+                                <figure>
+                                    <img
+                                        src={ prCoverWithCustomColor }
+                                        alt="PR Cover with Custom Color"
+                                    />
+                                    <figcaption>
+                                        PR Cover with Custom Color
+                                    </figcaption>
+                                </figure>
 
-                    <figure className={ "mx-lg-5 my-4" }>
-                        <img
-                            src={ releaseCoverInMathSweLegal }
-                            alt="Release Cover in MathSwe Legal"
-                        />
-                        <figcaption>Release Cover in MathSwe Legal</figcaption>
-                    </figure>
+                                <figure>
+                                    <img
+                                        src={ prCoverInMathSweOps }
+                                        alt="PR Cover in MathSwe Ops"
+                                    />
+                                    <figcaption>
+                                        PR Cover in MathSwe Ops
+                                    </figcaption>
+                                </figure>
+                            </div>
 
-                    <p>
-                        Release covers take the profile photo of the
-                        organization rather than the developer or team.
-                    </p>
+                            <div className="row">
+                                <figure>
+                                    <img
+                                        src={ releaseCoverInMathSweLegal }
+                                        alt="Release Cover in MathSwe Legal"
+                                    />
+                                    <figcaption>
+                                        Release Cover in MathSwe Legal
+                                    </figcaption>
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
 
-                    <p>
-                        Further, they support versions for micro-projects
-                        besides the version of the whole repository.
-                        Standalone projects of a common repository, like <b>
-                        microservices and MVPs, integrate smoothly</b> with their
-                        independent version.
-                    </p>
+                    <div className="wrap-width">
+                        <p>
+                            Release covers take the profile photo of the
+                            organization rather than the developer or team.
+                        </p>
 
-                    <p>
-                        Texsydo Web, with domain specifications, infers
-                        development context from PRs and releases and calls FX
-                        to streamline article graphics under MathSwe standards.
-                    </p>
+                        <p>
+                            Further, they support versions for micro-projects
+                            besides the version of the whole repository.
+                            Standalone projects of a common repository, like <b>
+                            microservices and MVPs, integrate smoothly</b> with
+                            their
+                            independent version.
+                        </p>
+
+                        <p>
+                            Texsydo Web, with domain specifications, infers
+                            development context from PRs and releases and calls
+                            FX
+                            to streamline article graphics under MathSwe
+                            standards.
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
