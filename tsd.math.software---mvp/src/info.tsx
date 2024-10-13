30c2ba34a2dd97180048b074e9db64396743da48
Tobias Briones <tbriones@mathswe.com>
2024-10-13 17:33:31 -0600

    Set project version to 0.1.0

--- tsd.math.software---mvp/src/info.tsx
+++ tsd.math.software---mvp/src/info.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // This file is part of https://github.com/texsydo/texsydo---mvp
 
-export const APP_SEMVER = "0.1.0-dev";
+export const APP_SEMVER = "0.1.0";
 export const APP_NAME = "Texsydo (Tsd.Math.Software)";
 export const APP_VERSION = `MVP v${ APP_SEMVER }`;
 export const APP_VERSION_LABEL = `${ APP_NAME } ${ APP_VERSION }`;
