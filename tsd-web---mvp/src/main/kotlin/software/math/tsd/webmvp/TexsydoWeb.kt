d777def74406327b9950139b13c5d065035c9f4c
Tobias Briones <tbriones@mathswe.com>
2025-08-21 22:16:55 -0600

    Remove CLI code from project
    Texsydo Web will be a separate library from its CLI, unlike the migrating prototype that coupled them all. It will allow development and deployment scalability while migrating Texsydo FX, Texsydo Dev (subset of TSD Web Prototype), etc.

--- tsd-web---mvp/src/main/kotlin/software/math/tsd/webmvp/TexsydoWeb.kt
+++ tsd-web---mvp/src/main/kotlin/software/math/tsd/webmvp/TexsydoWeb.kt
@@ -3,22 +3,3 @@
 // This file is part of https://github.com/texsydo/texsydo---mvp
 
 package software.math.tsd.webmvp
-
-import com.mathswe.kt.`$`
-import software.math.tsd.webmvp.fs.getRootPath
-
-fun main(args: Array<String>) {
-    if (args.isEmpty()) {
-        printError `$` "TSD Web received no arguments to run."
-        return
-    }
-
-    val arg: (Int) -> String = { args.getOrElse(it) { "" } }
-    val cmd = arg(0)
-    val root = getRootPath()
-        .onLeft { printError `$` "Failed to load project root path: $it" }
-        .getOrNull() ?: return
-
-        args.forEach { println(it) }
-    println("Running $cmd at $root")
-}
