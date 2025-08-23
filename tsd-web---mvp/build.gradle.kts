b30741fb9c0a1deb69cc9f4535e172bf24f90b52
Tobias Briones <tbriones@mathswe.com>
2025-08-23 14:42:00 -0600

    Move existing sources to library Gradle module web
    The previous code needs to return to its place after the initialization of the Gradle project `tsd-web---mvp` and its module `web` to continue with project operations.

--- tsd-web---mvp/build.gradle.kts
+++ tsd-web---mvp/build.gradle.kts
@@ -2,3 +2,11 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // This file is part of https://github.com/texsydo/texsydo---mvp
 
+allprojects {
+    group = "software.math.tsd.webmvp"
+    version = "0.1.0-dev"
+
+    repositories {
+        mavenCentral()
+    }
+}
