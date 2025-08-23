804196ce59ef44b27b24c762e659626fd808db64
Tobias Briones <tbriones@mathswe.com>
2025-08-23 13:40:29 -0600

    Fix default Gradle library structure in module web
    The project `tsd-web---mvp` is a multimodule Gradle project and the module `web` needs to adapt.

--- tsd-web---mvp/web/build.gradle.kts
+++ tsd-web---mvp/web/build.gradle.kts
@@ -6,25 +6,19 @@
  */
 
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     alias(libs.plugins.kotlin.jvm)
 
-    // Apply the java-library plugin for API and implementation separation.
     `java-library`
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
-    // Use the Kotlin JUnit 5 integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 
-    // Use the JUnit 5 integration.
     testImplementation(libs.junit.jupiter.engine)
-
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // This dependency is exported to consumers, that is to say found on their compile classpath.
@@ -34,7 +28,6 @@ dependencies {
     implementation(libs.guava)
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
@@ -42,6 +35,5 @@ java {
 }
 
 tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
