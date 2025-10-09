846f65c8d314ce5a0b4e16b5658e78eb7cb02a14
Tobias Briones <tbriones@mathswe.com>
2025-10-08 21:50:31 -0600

    Add MarkdownContent.kt with function Content.plainTextToMarkdownContent
    DoRep for Jekyll receives an intermediate Markdown file to generate the static website, so the flow will turn plain text articles into trivial Markdown content and then to static website via DoRep for Jekyll.

---
+++ tsd-web---mvp/web/src/main/kotlin/software/math/tsd/webmvp/md/MarkdownContent.kt
@@ -0,0 +1,20 @@
+// Copyright (c) 2025 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// This file is part of https://github.com/texsydo/texsydo---mvp
+
+package software.math.tsd.webmvp.md
+
+import software.math.tsd.webmvp.fromLowercaseToTitleCase
+import software.math.tsd.webmvp.io.Content
+import software.math.tsd.webmvp.io.title
+
+data class MarkdownContent(val value: String)
+
+fun Content.plainTextToMarkdownContent(): MarkdownContent =
+    MarkdownContent(
+        value = """
+            |# ${entry.title.fromLowercaseToTitleCase()}
+            |
+            |$content
+        """.trimMargin("|")
+    )
