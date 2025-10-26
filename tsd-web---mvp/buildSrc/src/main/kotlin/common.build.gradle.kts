31a918777d333d818a2614bbc2351e169c79acf1
Tobias Briones <tbriones@mathswe.com>
2025-10-25 23:31:49 -0600

    Set project tsd-web---mvp version to 0.1.0
    The project is ready to publish its initial  Text to Web feature.

--- tsd-web---mvp/buildSrc/src/main/kotlin/common.build.gradle.kts
+++ tsd-web---mvp/buildSrc/src/main/kotlin/common.build.gradle.kts
@@ -3,7 +3,7 @@
 // This file is part of https://github.com/texsydo/texsydo---mvp
 
 group = "software.math.tsd.webmvp"
-version = "0.1.0-dev"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
